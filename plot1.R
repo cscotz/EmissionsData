d1d98fcaa2a9d1203871b079fde57bd940381516
Chris Scott <cscott83@gmail.com>
2014-05-23 20:18:46 -0400

    Edit limits of y axis, add title

--- plot1.R
+++ plot1.R
@@ -7,8 +7,10 @@ agg <- tapply(NEI$Emissions,NEI$year,sum)
 #Plot data in a boxplot
 png(filename="plot1.png",  width = 480, height = 480)
 barplot(agg, 
-        yaxt="n", 
+        yaxt="n",
+        ylim=c(0,8e6),
         col=rainbow(4),
+        main="Emissions in the United States",
         xlab="Year",
         ylab="Total Emissions (million tons)")
 axis(2,at=c(0,2e6,4e6,6e6,8e6), labels=c(0,2,4,6,8))
